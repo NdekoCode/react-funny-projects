237194348c88bf75c25042a48841e9d24c0ecb1b
NdekoCode <arickbulakali@gmail.com>
2022-11-15 22:16:02 +0200

    feat: add boiling verdict for converter

--- src/pages/Converter.jsx
+++ src/pages/Converter.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import BoilingVerdict from "../components/converter/BoilingVerdict";
 
 const Converter = () => {
   return (
     <div className="container">
       <h1>Converter</h1>
+      <BoilingVerdict celsius={100} />
+      <BoilingVerdict celsius={50} />
     </div>
   );
 };
