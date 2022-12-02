1bd33c69c69b930ffae665ce5eb8f6da1a2cda34
Ndekocode <arickbulakali@gmail.com>
2022-12-02 16:12:22 +0200

    init: product page

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Formular from "./pages/Formular";
 import Home from "./pages/Home";
 import Incrementor from "./pages/Incrementor";
 import NotFound from "./pages/NotFound";
+import Products from "./pages/Products";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/increment" element={<Incrementor />} />
       <Route path="/form" element={<Formular />} />
       <Route path="/converter" element={<Converter />} />
-      <Route path="/" element={<Home />} />
+      <Route path="/produts" element={<Products />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
---
+++ src/pages/Products.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Product from "../components/productsFilter/Product";
+
+const Products = () => {
+  return (
+    <div>
+      <Product />
+    </div>
+  );
+};
+
+export default Products;
