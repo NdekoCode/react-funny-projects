b7842f61e73017f897cfef62ad21fd9ba01fb1b5
Ndekocode <arickbulakali@gmail.com>
2022-12-02 15:19:39 +0200

    refactor: temperature input with composition two column

--- src/components/converter/BoilingVerdict.jsx
+++ src/components/converter/BoilingVerdict.jsx
@@ -4,11 +4,11 @@ const BoilingVerdict = ({ celsius }) => {
   return (
     <>
       {celsius >= 100 ? (
-        <div className="p-2 my-1 rounded bg-green-200 text-gray-800">
+        <div className="p-2.5 py-3 my-1 rounded bg-green-200 text-green-900">
           L'eau bout
         </div>
       ) : (
-        <div className="px-2 py-2.5 my-1 rounded bg-red-200 text-red-800">
+        <div className="px-2.5 py-3 my-1 text-sm rounded bg-red-200 text-red-800">
           L'eau ne bout pas
         </div>
       )}
--- src/pages/Converter.jsx
+++ src/pages/Converter.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Calculator from "../components/converter/Calculator";
-
 const Converter = () => {
   return (
-    <div className="container">
-      <h1 className="text-4xl font-bold text-gray-700">Converter</h1>
+    <div className="max-w-7xl mx-auto my-3">
+      <h1 className="text-4xl font-bold text-gray-700 my-3">
+        Converter (Temperature Celcius/ Fahrenheit)
+      </h1>
       <div className="max-w-7xl mx-auto p-5 shadow bg-white rounded-lg">
         <Calculator />
       </div>
