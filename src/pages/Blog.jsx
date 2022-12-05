1b86081daf0f5bb2cccac40ae962ae44efe0a00a
Ndekocode <arickbulakali@gmail.com>
2022-12-05 07:52:12 +0200

    load api comment data with custom hook

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Comment from "../components/blog/Comment";
 import TodoList from "../components/blog/TodoList";
 const Blog = () => {
   const [posts, setPosts] = useState([]);
@@ -6,6 +7,7 @@ const Blog = () => {
     <div className="max-w-7xl mx-auto my-3">
       <h1 className="text-4xl font-bold text-gray-700 my-3">Mon blog</h1>
       <TodoList />
+      <Comment />
     </div>
   );
 };
