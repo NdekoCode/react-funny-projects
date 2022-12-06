a923345bad98de677da373e549d4ebc13c597afd
Ndekocode <arickbulakali@gmail.com>
2022-12-06 05:08:35 +0200

    feat: great incrementor with useReducer

--- src/components/Incrementor/ManuelIncrement.jsx
+++ src/components/Incrementor/ManuelIncrement.jsx
@@ -1,7 +1,4 @@
 import { useEffect, useReducer, useState } from "react";
-import AutoCompter from "./AutoCompter";
-import Compter from "./Compter";
-import ToggleCompter from "./ToggleCompter";
 function reducer(state, action) {
   switch (action.type) {
     case "increment":
@@ -10,6 +7,8 @@ function reducer(state, action) {
       return state - 1;
     case "reset":
       return 0;
+    case "pause":
+      return state;
     default:
       throw new Error(
         "Impossible de faire des operation avec cette action qui est inconnu"
@@ -17,52 +16,43 @@ function reducer(state, action) {
   }
 }
 const ManuelIncrement = () => {
-  const [timer, setTimer] = useState();
+  const [timer, setTimer] = useState(null);
   // useReducer retourner la valeur de l'etat mais aussi un dispatcher: un dispatcher est une fonction qui devra etre appeler et à qui on passera une action, cette action permettra de declencher un type de mutation particulier
   const [count, dispatch] = useReducer(reducer, 0);
-  const [state, setState] = useState({ count: 0, timer: null });
-  const increment = () => {
-    setState((state) => ({ ...state, count: state.count + 1 }));
-  };
   const pause = () => {
-    setState((state) => ({
-      ...state,
-      timer: window.clearInterval(state.timer),
-    }));
+    console.log(timer);
+    window.clearInterval(timer);
+    setTimer(null);
   };
   const play = () => {
     pause();
-    const timer = window.setInterval(increment, 1000);
-    setState((state) => ({
-      ...state,
-      timer,
-    }));
+    setTimer(window.setInterval(() => dispatch({ type: "increment" }), 1000));
   };
-  const method = state.timer ? pause : play;
-  const text = state.timer ? "Pause" : "Play";
+  const method = timer ? pause : play;
+  const text = timer ? "Pause" : "Play";
   const reset = () => {
-    setState((state) => ({
-      ...state,
-      count: 0,
-      timer: window.clearInterval(state.timer),
-    }));
+    dispatch({ type: "reset" });
+    pause();
     play();
   };
   useEffect(() => {
-    play();
     return () => {
-      window.clearInterval(state.timer);
+      window.clearInterval(timer);
+      play();
     };
   }, []);
   return (
     <div className="px-2 py-3 shadow">
-      <button onClick={dispatch({ type: "increment" })}>
-        Incrementer {count}
+      <button onClick={() => dispatch({ type: "increment" })}>
+        UseReducer Incrementer {count}
       </button>
-      <ToggleCompter />
-      <Compter initial={3} step={5} />
-      <AutoCompter />
-      {state.count} <button onClick={increment}>Incrementer</button>
+      {/* <ToggleCompter /> */}
+      {/* <Compter initial={3} step={5} /> */}
+      {/* <AutoCompter /> */}
+      {count}{" "}
+      <button onClick={() => dispatch({ type: "increment" })}>
+        Incrementer
+      </button>
       <div className="flex items-center justify-center mt-5">
         <button onClick={method}>{text}</button>
         <button onClick={reset}>Reset</button>
