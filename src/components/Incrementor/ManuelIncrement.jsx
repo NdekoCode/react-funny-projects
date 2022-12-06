e4750f8a536182c0ee5ebaa07aae4c98dc017565
Ndekocode <arickbulakali@gmail.com>
2022-12-06 05:38:48 +0200

    render maximum compter

--- src/components/Incrementor/ManuelIncrement.jsx
+++ src/components/Incrementor/ManuelIncrement.jsx
@@ -1,24 +1,35 @@
 import { useEffect, useReducer, useState } from "react";
+import AutoCompter from "./AutoCompter";
+import Compter from "./Compter";
+import ToggleCompter from "./ToggleCompter";
 function reducer(state, action) {
   switch (action.type) {
     case "increment":
-      return state + 1;
+      return { count: state.count + action.payload || 1 };
     case "decrement":
-      return state - 1;
+      if (state > 0) {
+        return { count: state.count - action.payload || 1 };
+      }
+      return state.count;
     case "reset":
-      return 0;
+      return init(0);
     case "pause":
-      return state;
+      return state.count;
     default:
       throw new Error(
         "Impossible de faire des operation avec cette action qui est inconnu"
       );
   }
 }
+function init(initialValue) {
+  return {
+    count: initialValue,
+  };
+}
 const ManuelIncrement = () => {
   const [timer, setTimer] = useState(null);
   // useReducer retourner la valeur de l'etat mais aussi un dispatcher: un dispatcher est une fonction qui devra etre appeler et à qui on passera une action, cette action permettra de declencher un type de mutation particulier
-  const [count, dispatch] = useReducer(reducer, 0);
+  const [state, dispatch] = useReducer(reducer, 0, init);
   const pause = () => {
     console.log(timer);
     window.clearInterval(timer);
@@ -43,13 +54,16 @@ const ManuelIncrement = () => {
   }, []);
   return (
     <div className="px-2 py-3 shadow">
-      <button onClick={() => dispatch({ type: "increment" })}>
-        UseReducer Incrementer {count}
+      <button onClick={() => dispatch({ type: "increment", payload: 1 })}>
+        UseReducer Incrementer {state.count}
       </button>
-      {/* <ToggleCompter /> */}
-      {/* <Compter initial={3} step={5} /> */}
-      {/* <AutoCompter /> */}
-      {count}{" "}
+      <button onClick={() => dispatch({ type: "decrement" })}>
+        Decrement {state.count}
+      </button>
+      <ToggleCompter />
+      <Compter initial={3} step={5} />
+      <AutoCompter />
+      {state.count}
       <button onClick={() => dispatch({ type: "increment" })}>
         Incrementer
       </button>
