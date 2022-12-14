702b4e18b4fc9e8131677de717a3dc851754266e
Ndekocode <arickbulakali@gmail.com>
2022-12-14 13:42:25 +0200

    add toggle theme icon

--- src/components/opt/Profile.jsx
+++ src/components/opt/Profile.jsx
@@ -1,27 +1,71 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { memo, useCallback, useEffect, useMemo, useState } from "react";
 import { useIncrement } from "../../hooks/useUtils";
 import TableUser from "./TableUser";
-const Profile = () => {
+const Profile = memo(() => {
   const [count, increnement] = useIncrement(1);
   const [user, setUser] = useState({});
   const [userLoading, setLoading] = useState(true);
   const [isDark, setIsDark] = useState(false);
-  const handleTheme = () => {
+  const theme = useMemo(
+    () => (isDark ? "dark-theme" : "light-theme"),
+    [isDark]
+  );
+  const buttonText = useMemo(() =>
+    isDark ? (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="ionicon h-4 w-4 fill-current"
+        viewBox="0 0 512 512"
+      >
+        <title>Moon</title>
+        <path
+          d="M160 136c0-30.62 4.51-61.61 16-88C99.57 81.27 48 159.32 48 248c0 119.29 96.71 216 216 216 88.68 0 166.73-51.57 200-128-26.39 11.49-57.38 16-88 16-119.29 0-216-96.71-216-216z"
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={32}
+        />
+      </svg>
+    ) : (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="ionicon h-4 w-4 fill-current"
+        viewBox="0 0 512 512"
+      >
+        <title>Sunny</title>
+        <path
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          strokeWidth={32}
+          d="M256 48v48M256 416v48M403.08 108.92l-33.94 33.94M142.86 369.14l-33.94 33.94M464 256h-48M96 256H48M403.08 403.08l-33.94-33.94M142.86 142.86l-33.94-33.94"
+        />
+        <circle
+          cx={256}
+          cy={256}
+          r={80}
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          strokeWidth={32}
+        />
+      </svg>
+    )
+  );
+  const handleTheme = useCallback(() => {
     setIsDark((d) => !d);
     document.body.className = theme;
-  };
-  const theme = isDark ? "dark-theme" : "light-theme";
-  const isEnderTen = useMemo(() => count < 10);
+  }, [theme]);
+  const isEnderTen = useMemo(() => count < 10, [count]);
   useEffect(() => {
-    let controller = new AbortController();
-    let signal = controller.signal;
     (async () => {
       const response = await fetch(
-        `https://jsonplaceholder.typicode.com/users/${count}`,
-        { signal: signal }
+        `https://jsonplaceholder.typicode.com/users/${count}`
       );
       const responseData = await response.json();
-      console.log("render");
       if (response.ok) {
         setLoading(false);
         setUser(responseData);
@@ -29,9 +73,6 @@ const Profile = () => {
         setLoading(false);
       }
     })();
-    return () => {
-      controller.abort();
-    };
   }, [count]);
 
   return (
@@ -62,7 +103,7 @@ const Profile = () => {
           onClick={handleTheme}
           className="px-3 py-2 rounded-lg bg-gray-900 text-white ml-auto"
         >
-          Changer
+          {buttonText}
         </button>
       </div>
       {userLoading ? (
@@ -74,6 +115,6 @@ const Profile = () => {
       )}
     </div>
   );
-};
+});
 
 export default Profile;
