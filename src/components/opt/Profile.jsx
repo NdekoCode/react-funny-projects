b3f05fc2bda1ed818673e5d8ba6892c2f3dd121b
Ndekocode <arickbulakali@gmail.com>
2022-12-14 11:39:37 +0200

    get single user

--- src/components/opt/Profile.jsx
+++ src/components/opt/Profile.jsx
@@ -1,18 +1,11 @@
-import React, { useEffect, useState } from "react";
-
+import React from "react";
+import { useFetch, useIncrement } from "../../hooks/useUtils";
 const Profile = () => {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/users"
-      );
-      const responseData = await response.json();
-      if (response.ok) {
-        setUsers(responseData);
-      }
-    })();
-  });
+  const [count, increnement] = useIncrement(1);
+  const [user, userLoading] = useFetch(
+    `https://jsonplaceholder.typicode.com/users/${count}`
+  );
+
   return (
     <div className="mt-5 w-full max-w-7xl mx-auto px-3">
       <h1 className="font-bold text-4xl my-3">useMemo</h1>
@@ -29,49 +22,64 @@ const Profile = () => {
         </div>
       </form>
       <div className="my-3 flex items-center justify-between">
-        <button className="px-3 py-2 rounded-lg bg-blue-600">decrement</button>
-        <button className="px-3 py-2 rounded-lg bg-yellow-600">
-          increment
+        <button
+          className="px-3 py-2 rounded-lg bg-blue-700 text-white"
+          onClick={increnement}
+        >
+          Increment {count}
+        </button>
+        <button className="px-3 py-2 rounded-lg bg-gray-900 text-white">
+          Changer
         </button>
       </div>
-      <table className="table-auto text-gray-500 w-full text-sm border rounded-lg shadow-md">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50">
-          <tr className="border-b">
-            <th scope="col" className="py-3 px-6">
-              #ID
-            </th>
-            <th scope="col" className="py-3 px-6">
-              Nom
-            </th>
-            <th scope="col" className="py-3 px-6">
-              Pseudo
-            </th>
-            <th scope="col" className="py-3 px-6">
-              Email
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr className="bg-gray-50 border-b text-center border-b">
-              <td
-                scope="row"
-                className="py-4 px-6 font-medium whitespace-nowrap text-gray-900"
-              >
-                {user.id}
-              </td>
-              <td
-                scope="row"
-                className="py-4 px-6 font-medium whitespace-nowrap text-gray-900"
-              >
-                {user.name}
-              </td>
-              <td>{user.username}</td>
-              <td>{user.email}</td>
+      {userLoading ? (
+        <div className="min-h-screen w-full flex items-center justify-center">
+          <p>Loading...</p>
+        </div>
+      ) : (
+        <table className="table-auto text-gray-500 w-full text-sm border rounded-lg shadow-md">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+            <tr className="border-b">
+              <th scope="col" className="py-3 px-6">
+                #ID
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Nom
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Pseudo
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Email
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {user && (
+              <tr
+                className={`${
+                  user.id % 2 === 0 ? "bg-gray-50" : "bg-white"
+                } border-b text-center`}
+              >
+                <td
+                  scope="row"
+                  className="py-4 px-6 font-medium whitespace-nowrap text-gray-900"
+                >
+                  {user.id}
+                </td>
+                <td
+                  scope="row"
+                  className="py-4 px-6 font-medium whitespace-nowrap text-gray-900"
+                >
+                  {user.name}
+                </td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
--- src/hooks/useUtils.js
+++ src/hooks/useUtils.js
@@ -1,17 +1,17 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export function useIncrement(initial = 0, step = 1) {
   const [count, setCount] = useState(initial);
-  const increment = () => {
+  const increment = useCallback(() => {
     setCount((c) => c + step);
-  };
+  });
   return [count, increment];
 }
 export function useIsVisible(initial = true) {
   const [isVisible, setIsVisible] = useState(initial);
-  const toggleVisible = () => {
+  const toggleVisible = useCallback(() => {
     setIsVisible((v) => !v);
-  };
+  });
   return [isVisible, toggleVisible];
 }
 export function useAutoIncrement(initial = 0, step = 1) {
@@ -40,11 +40,29 @@ export function useFetch(url) {
         setState((d) => ({ ...d, loading: false }));
       }
     })();
+    console.log(state.items);
   }, []);
   return [state.items, state.loading];
 }
+export function useFetchData(url, setData) {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(url);
+      const responseData = await response.json();
+      if (response.ok) {
+        setLoading(false);
+        setData(responseData);
+      } else {
+        setLoading(false);
+      }
+      console.log(responseData);
+    })();
+  }, []);
+  return [loading, setLoading];
+}
 export function useVisible() {
   const [visible, setVisible] = useState(false);
-  const handleVisible = () => setVisible((d) => !d);
+  const handleVisible = useCallback(() => setVisible((d) => !d));
   return [visible, handleVisible];
 }
